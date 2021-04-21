f1ec7232ef481ed26d6667068f7d7a44cc8de8a2
AleksunderVolkov <alexander.volkov@rochev.studio>
2021-04-21 22:22:18 +0300

    WIP

--- AReality/ContentView.swift
+++ AReality/ContentView.swift
@@ -10,7 +10,24 @@ import RealityKit
 
 struct ContentView : View {
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack {
+            ARViewContainer().edgesIgnoringSafeArea(.all)
+        VStack {
+           Spacer()
+           Spacer()
+//            Button(action: {
+//                print("Edit button was tapped")
+//            }) {
+//                Image("TankRight")
+//                    
+//            }.padding()
+            .background(RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color.white).opacity(0.7))
+        }
+     }
+        
+        
+       
     }
 }
 
@@ -22,6 +39,7 @@ struct ARViewContainer: UIViewRepresentable {
         
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
+//        let tankAnchor = try! TinyToyTank.load_TinyToyTank()
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
